8f2ce770eaacb413e5c5918d9dac17b3cbad0607
ABoileauGIT <2393443@csfoy.ca>
2023-09-03 10:29:45 -0400

    Ajout -> Update des stock dans la BD et unset de $_SESSION['cart'] lors de la confirmation.

--- classes/cartDAO.class.php
+++ classes/cartDAO.class.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 class CartDAO
@@ -15,17 +16,18 @@ class CartDAO
         $this->db = $db;
     }
 
-    public function hasEnoughStockForCart(Cart $cart) : bool{
+    public function hasEnoughStockForCart(Cart $cart): bool
+    {
         foreach ($cart->getCartItems() as $item) {
-            $product = $item->getProduct();
-            $sku = $product->getSku();
             $quantity = $item->getProductQuantity();
+
             $req = $this->db->prepare('SELECT stock FROM products WHERE sku = :sku');
-            $req->bindValue(':sku', $sku, PDO::PARAM_INT);
+            $req->bindValue(':sku',  $item->getProductSku(), PDO::PARAM_INT);
             $req->execute();
+
             $line = $req->fetch(PDO::FETCH_ASSOC);
             $qtyInStock = $line['stock'];
-            if($qtyInStock<$quantity){
+            if ($qtyInStock < $quantity) {
                 return false;
             }
             $req->closeCursor();
@@ -38,13 +40,12 @@ class CartDAO
             (SELECT id FROM users WHERE email=:email), NOW())');
         $req->bindValue(':email', $cart->getEmail(), PDO::PARAM_STR);
         $req->execute();
-        $orderId = $this -> db->lastInsertId();
+        $orderId = $this->db->lastInsertId();
 
-        foreach($cart->getCartItems() as $cartItem) {
+        foreach ($cart->getCartItems() as $cartItem) {
             $req = $this->db->prepare('INSERT INTO order_items(order_id,product_sku,quantity)
                 VALUES (:order_id, :product_sku, :quantity)');
 
-                
             $req->bindValue(':order_id', $orderId, PDO::PARAM_INT);
             $req->bindValue(':product_sku', $cartItem->getProductSku(), PDO::PARAM_INT);
             $req->bindValue(':quantity', $cartItem->getProductQuantity(), PDO::PARAM_INT);
@@ -52,5 +53,28 @@ class CartDAO
         }
         $req->closeCursor();
     }
+
+    public function adjustStockQuantities(Cart $cart): void
+    {
+        foreach ($cart->getCartItems() as $item) {
+            
+            $qtyOrdered = $item->getProductQuantity();
+
+            $req = $this->db->prepare('SELECT stock FROM products WHERE sku = :sku');
+            $req->bindValue(':sku',  $item->getProductSku(), PDO::PARAM_INT);
+            $req->execute();
+
+            if($line = $req->fetch(PDO::FETCH_ASSOC)){
+                $qtyInStock = $line['stock'];
+                $newStockQty = $qtyInStock - $qtyOrdered;
+                $req->closeCursor();
+                
+                $req = $this->db->prepare('UPDATE products SET stock = :stock WHERE sku = :sku');
+                $req->bindValue(':stock', $newStockQty, PDO::PARAM_INT);
+                $req->bindValue(':sku', $item->getProductSku(), PDO::PARAM_INT);
+                $req->execute();
+            }
+            $req->closeCursor();
+        }
+    }
 }
-?>
--- confirm.php
+++ confirm.php
@@ -1,21 +1,27 @@
 <?php
 session_start();
 require_once('classes/cart.class.php');
-require_once('includes/connection.php');
 require_once('classes/cartDAO.class.php');
+require_once('includes/connection.php');
 $fileName = '';
 
 if(!isset($_SESSION['checkoutConfirmation'])){
     header('Location: index.php');
     exit();
 }
-unset($_SESSION['checkoutConfirmation']);
+
 $cartDao = new CartDAO($conn);
 $cart = unserialize($_SESSION['cart']);
+unset($_SESSION['checkoutConfirmation']);
+unset($_SESSION['cart']); // Reinitialize le cart un fois la commande confirmee et ajuste les products stock dans la $_SESSION['cart'] provenant de la BD a jour)
 if($cartDao->hasEnoughStockForCart($cart)){
     $cartDao->addOrder($cart);
+    $cartDao->adjustStockQuantities($cart);
 }
-unset($_SESSION['cart']);
+else{
+    echo "Inventaire insuffisant depuis l'ajout au panier. 
+    Veillez svp ajouter vos items de nouveau, les quantitee en stock sont maintenant a jour."; //TODO: affichage erreur conflit d'inventaire
+}
 $conn = null; 
 ?>
 <!DOCTYPE html>
@@ -25,8 +31,8 @@ $conn = null;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Confirmation | Maverick Custom Shop</title>
-    <?php require_once('includes/head.php'); ?>
     <script defer src="js/confirm.js"></script>
+    <?php require_once('includes/head.php'); ?>
 </head>
 
 <body>
@@ -36,7 +42,9 @@ $conn = null;
 
     <main class="container-fluid">
         <p> Merci pour votre commande ! </p>
-        <button id="btnReturn" class="btn btn-outline-primary">Continuer de magasiner</button>
+        <form action="confirm.php">
+            <button class="btn btn-outline-primary">Continuer de magasiner</button>
+        </form>
     </main>
     <footer>
         <?php include('includes/footer.php'); ?>
